5c89dc4ba2e43ef7610a2401d92e12e75143fae8
bilykb <brettbilyk@gmail.com>
2021-07-01 23:21:47 +0000

    initial commit with all my work for Javascripting programme

---
+++ introduction.js
@@ -0,0 +1,1 @@
+console.log('hello'); // prints to console hello
---
+++ looping-through-arrays.js
@@ -0,0 +1,7 @@
+let pets = ['cat', 'dog', 'rat']
+let arrL = pets.length
+
+for (let i = [0]; i < arrL; i++) {
+  pets[i] = pets[i] + 's'
+}
+console.log(pets)
